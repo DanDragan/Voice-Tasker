4e359546e71b5e208c565bc20dc6c3a989918e22
Dan Dragan <dan.dragan1992@gmail.com>
2013-06-28 11:56:05 +0300

    Close to MVP

--- src/ncit/android/voicetasker/Activity_Voice.java
+++ src/ncit/android/voicetasker/Activity_Voice.java
@@ -9,76 +9,94 @@ import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.Menu;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
- 
+
 public class Activity_Voice extends Activity {
- 
-    protected static final int RESULT_SPEECH = 1;
- 
-    private ImageButton btnSpeak;
-    private TextView txtText;
- 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_voice);
- 
-        txtText = (TextView) findViewById(R.id.txtText);
- 
-        btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
- 
-        btnSpeak.setOnClickListener(new View.OnClickListener() {
- 
-            @Override
-            public void onClick(View v) {
- 
-                Intent intent = new Intent(
-                        RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
- 
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
- 
-                try {
-                    startActivityForResult(intent, RESULT_SPEECH);
-                    txtText.setText("");
-                } catch (ActivityNotFoundException a) {
-                    Toast t = Toast.makeText(getApplicationContext(),
-                            "Opps! Your device doesn't support Speech to Text",
-                            Toast.LENGTH_SHORT);
-                    t.show();
-                }
-            }
-        });
- 
-    }
- 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
- 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
- 
-        switch (requestCode) {
-        case RESULT_SPEECH: {
-            if (resultCode == RESULT_OK && data != null) {
- 
-                ArrayList<String> text = data
-                        .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
- 
-                txtText.setText(text.get(0));
-            }
-            break;
-        }
- 
-        }
-    }
-    
-    protected String getText() {
-    	return txtText.toString();
-    }
+
+	protected static final int RESULT_SPEECH = 1;
+
+	private Button btnSpeak;
+	private Button btnReset;
+	private Button btnExit;
+	private ListView lView;
+	private int index;
+	ArrayAdapter<String> adapter;
+	ArrayList<String> list;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_voice);
+
+		lView = (ListView) findViewById(R.id.listview);
+		index = 0;
+
+		btnSpeak = (Button) findViewById(R.id.btnSpeak);
+		btnReset = (Button) findViewById(R.id.btnReset);
+		btnExit = (Button) findViewById(R.id.btnExit);
+
+		list = new ArrayList<String>();
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, list);
+		lView.setAdapter(adapter);
+
+		btnSpeak.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				Intent intent = new Intent(
+						RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+				intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+
+				try {
+					startActivityForResult(intent, RESULT_SPEECH);
+				} catch (ActivityNotFoundException a) {
+					Toast t = Toast.makeText(getApplicationContext(),
+							"Opps! Your device doesn't support Speech to Text",
+							Toast.LENGTH_SHORT);
+					t.show();
+				}
+			}
+		});
+
+	}
+
+	private void addItems(String item) {
+
+		if (item.length() > 0) {
+			this.list.add(item);
+			this.adapter.notifyDataSetChanged();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		switch (requestCode) {
+		case RESULT_SPEECH: {
+			if (resultCode == RESULT_OK && data != null) {
+
+				ArrayList<String> text = data
+						.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+				this.addItems(text.get(index));
+				index++;
+			}
+			break;
+		}
+
+		}
+	}
 }
