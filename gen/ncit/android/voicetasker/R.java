a2a6c69f6bb9480a45f8906e33c27432eecc2ef5
Alexandru Zarnescu <alexandru.zarnescu@gmail.com>
2013-07-08 09:50:04 +0300

    portrait mode

--- gen/ncit/android/voicetasker/R.java
+++ gen/ncit/android/voicetasker/R.java
@@ -24,20 +24,19 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int background=0x7f020000;
-        public static final int background1=0x7f020001;
-        public static final int background2=0x7f020002;
-        public static final int background3=0x7f020003;
-        public static final int background4=0x7f020004;
-        public static final int background5=0x7f020005;
-        public static final int background6=0x7f020006;
-        public static final int ic_launcher=0x7f020007;
-        public static final int icon=0x7f020008;
-        public static final int images=0x7f020009;
-        public static final int paperclip=0x7f02000a;
-        public static final int postit_transp1=0x7f02000b;
-        public static final int postit_transp2=0x7f02000c;
-        public static final int postit_transp3=0x7f02000d;
+        public static final int background1=0x7f020000;
+        public static final int background2=0x7f020001;
+        public static final int background3=0x7f020002;
+        public static final int background4=0x7f020003;
+        public static final int background5=0x7f020004;
+        public static final int background6=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int icon=0x7f020007;
+        public static final int images=0x7f020008;
+        public static final int paperclip=0x7f020009;
+        public static final int postit_transp1=0x7f02000a;
+        public static final int postit_transp2=0x7f02000b;
+        public static final int postit_transp3=0x7f02000c;
     }
     public static final class id {
         public static final int action_settings=0x7f090011;
